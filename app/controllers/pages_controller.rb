fe7622969aa89b5160305dc6932a61debcaa7115
grantkee <grant.d.kee@gmail.com>
2021-01-18 21:22:57 -0600

    setup alternative about page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,4 +5,7 @@ class PagesController < ApplicationController
 
   def about
   end
+
+  def about_alt
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'pages#welcome'
 
   get '/about' => 'pages#about'
+  get '/about_alt' => 'pages#about_alt'
 end

fe3b5f2cdaa3afe23e624ea598c536feda83d9b7
grantkee <grant.d.kee@gmail.com>
2021-01-28 11:53:37 -0600

    set root to about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  root 'pages#about'
 
-  get '/about' => 'pages#about'
   get '/about_alt' => 'pages#about_alt'
 
   get '/projects' => 'pages#projects'
